96eb909343c5bf2642f9309633b3d86345e952e1
11292-stella <stella.marucelli@gmail.com>
2025-03-18 16:50:06 +0100

    session storage

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ setInterval(function () {
   numCont.appendChild(newDiv)
   counter++
 }, 1000)
+
+let startTime = Date.now()
+
+function piutime() {
+  const elTime = Math.floor(Date.now() - startTime / 1000)
+  const nDiv = document.createElement("div")
+  nDiv.innerText = counter + " (tempo trascorso:" + elTime + "secondi)"
+  numCont.appendChild(nDiv)
+
+  sessionStorage.setItem("counter", counter)
+  counter++
+}
+document.getElementById("updateButton").addEventListener("click", piutime)
